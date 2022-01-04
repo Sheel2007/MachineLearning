87e0f223bdfcb8cb51507a9b04bea53372132d11
Sheel2007 <sheel_arsenal@icloud.com>
2022-01-04 17:58:47 -0500

    Data Splitting

--- salary.py
+++ salary.py
@@ -1,6 +1,13 @@
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('Levels_Fyi_Salary_Data.csv')
 
 df = df[['company', 'totalyearlycompensation', 'yearsofexperience', 'yearsatcompany', 'basesalary']]
+
+x = df.drop('totalyearlycompensation', axis=1)
+y = df['totalyearlycompensation']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
