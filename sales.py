4ab4490fb0915dbf7de1844bd62b4f86140880ec
Sheel2007 <sheel_arsenal@icloud.com>
2022-01-06 12:51:29 -0500

    Data Splitting

--- sales.py
+++ sales.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('supermarket_sales - Sheet1.csv')
 
@@ -13,3 +14,9 @@ df['City'] = df['City'].replace({'Yangon': 0, 'Naypyitaw': 1, 'Mandalay': 2})
 df['Customer type'] = df['Customer type'].replace({'Normal': 0, 'Member': 1})
 df['Gender'] = df['Gender'].replace({'Female': 0, 'Male': 1})
 df['Payment'] = df['Payment'].replace({'Cash': 0, 'Credit card': 1, 'Ewallet': 2})
+
+x = df.drop('Rating', axis=1)
+y = df['Rating']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
