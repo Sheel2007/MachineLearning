1c23510c1b4367fca94135b69f48d508b2fce1df
Sheel2007 <sheel_arsenal@icloud.com>
2022-01-06 12:53:00 -0500

    Making Model

--- sales.py
+++ sales.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 df = pd.read_csv('supermarket_sales - Sheet1.csv')
 
@@ -20,3 +21,5 @@ y = df['Rating']
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
+lr = LinearRegression()
+lr.fit(x_train, y_train)
