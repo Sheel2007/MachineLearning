a7a6a5c97ffa5e1c86903c3a141fd41caedf7e56
Sheel2007 <sheel_arsenal@icloud.com>
2022-01-06 12:43:30 -0500

    Removing Columns

--- sales.py
+++ sales.py
@@ -3,4 +3,7 @@ import numpy as np
 
 df = pd.read_csv('supermarket_sales - Sheet1.csv')
 
-print(df.head())
+df = df.drop('Invoice ID', axis=1)
+df = df.drop('Product line', axis=1)
+df = df.drop('Date', axis=1)
+df = df.drop('Time', axis=1)
