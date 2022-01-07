5f062b9bd1c9764b50c1157bb65c5aeb6f6c861a
Sheel2007 <sheel_arsenal@icloud.com>
2022-01-07 08:53:25 -0500

    Building Model

--- heart_failure.py
+++ heart_failure.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
 
 df = pd.read_csv('heart_failure_clinical_records_dataset.csv')
 
@@ -12,3 +14,12 @@ for i, _ in enumerate(color_num):
     plt.scatter(df['age'][i], df['platelets'][i], c=color_num[i])
 
 plt.show()
+
+x = df.drop('DEATH_EVENT', axis=1)
+y = df['DEATH_EVENT']
+
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+clf = SVC()
+clf.fit(x_train, y_train)
