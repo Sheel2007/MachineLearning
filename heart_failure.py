2b83124d6368dd789ea5ffd49f7348fb652e0b0a
Sheel2007 <sheel_arsenal@icloud.com>
2022-01-07 08:49:27 -0500

    Plotting Values

--- heart_failure.py
+++ heart_failure.py
@@ -1,5 +1,14 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('heart_failure_clinical_records_dataset.csv')
-print(df.head())
+
+colors = ['green', 'red']
+
+color_num = [str(colors[i]) for i in df['DEATH_EVENT']]
+
+for i, _ in enumerate(color_num):
+    plt.scatter(df['age'][i], df['platelets'][i], c=color_num[i])
+
+plt.show()
